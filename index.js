3561be04d8a17d261de8d574fbec82719ca6dddf
longvb <vuongbaolong48@gmail.com>
2020-08-17 15:14:41 +0700

    fix 99% cpu

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 const express = require("express");
 const crawl = require("./web-crawler");
-const path = require('path');
+const path = require("path");
 const app = express();
 const port = 3001;
+const puppeteer = require("puppeteer");
 
-app.get("/get-html", async (req, res) => {
-    const url = req.query["url"];
-
+async function main() {
     try {
-        const html = await crawl(url);
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        console.log("lanch puppeteer");
 
-        res.jsonp({
-            success: true,
-            status: 200,
-            data: {
-                url: url,
-                html: html,
-            },
-            message: "ok",
+        app.get("/get-html", async (req, res) => {
+            const url = req.query["url"];
+
+            try {
+                const html = await crawl(page, url);
+
+                res.jsonp({
+                    success: true,
+                    status: 200,
+                    data: {
+                        url: url,
+                        html: html,
+                    },
+                    message: "ok",
+                });
+            } catch (error) {
+                res.status(400).jsonp({
+                    success: false,
+                    status: 400,
+                    data: null,
+                    message: "Bad Request",
+                });
+            }
         });
-    } catch (error) {
-        res.status(400).jsonp({
-            success: false,
-            status: 400,
-            data: null,
-            message: "Bad Request",
+
+        app.get("/", (req, res) => {
+            res.sendFile(path.join(__dirname + "/views/index.html"));
         });
+
+        app.listen(port, () => {
+            console.log(`Example app listening at http://localhost:${port}`);
+        });
+    } catch (e) {
+        console.error(e);
     }
-});
+}
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/index.html"));
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+main();
--- web-crawler.js
+++ web-crawler.js
@@ -1,19 +1,21 @@
 const puppeteer = require("puppeteer");
 
-function crawl(url) {
+function crawl(page, url) {
     // url = "https://lausamplus.labo.io/?blockJs=1"
     return new Promise(async (resolve, reject) => {
         try {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
+            // const browser = await puppeteer.launch();
+            // console.log("lanch puppeteer")
+            // const page = await browser.newPage();
             await page.goto(url);
             let urls = await page.evaluate(() => {
                 const html = new XMLSerializer().serializeToString(document);
                 return html;
             });
-            browser.close();
+
             return resolve(urls);
         } catch (e) {
+            console.log(e);
             return reject(e);
         }
     });
